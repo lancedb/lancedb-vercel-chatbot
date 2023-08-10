c3602387bb01b6731ec03201b9b46395f12bd6df
Tevin Wang <tevin@cmu.edu>
2023-08-10 00:01:15 -0700

    Make sure binary gets seen during static file analysis (#5)
    * vercel compatibility
    
    * correct next config and add scrape via cheerio
    
    * Cleanup and use sed

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = ({
-  experimental: {
-    outputFileTracingIncludes: {
-      "src/app/api/**": [
-          "node_modules/@lancedb/vectordb-linux-x64-gnu",
-      ],
-  },
-  },
-  webpack(config, { isServer }) {
+  webpack(config) {
     config.externals.push({ vectordb: 'vectordb' })
     return config;
   }
